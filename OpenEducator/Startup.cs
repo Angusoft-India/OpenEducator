fcadbe7acdfe0867db152733e1e084a661164403
aryan-mann <aryan@aryanmann.com>
2017-01-06 00:24:49 +0530

    Add project files.

---
+++ OpenEducator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenEducator.Startup))]
+namespace OpenEducator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
